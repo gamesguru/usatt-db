e06b828db000aae7657fd62248cc8f757dd0dbf6
gamesguru <mathmuncher11@gmail.com>
2019-10-01 23:33:37 -0400

    add game_id to ratings table, as each rating is associated to a new game

--- players.sql
+++ players.sql
@@ -1,5 +1,5 @@
 
--- Main users table --
+-- Main users table
 CREATE TABLE players.users(
     user_id SERIAL PRIMARY KEY,
     username CITEXT NOT NULL,
@@ -8,6 +8,9 @@ CREATE TABLE players.users(
     email CITEXT,
     email_token_activate VARCHAR(200),
     email_token_pw_reset VARCHAR(200),
+    cdsid VARCHAR(40),
+    name VARCHAR(90),
+    rating float,
     gender VARCHAR(20),
     height SMALLINT,
     grip VARCHAR(20),
@@ -18,27 +21,38 @@ CREATE TABLE players.users(
     UNIQUE(unverified_email)
 );
 
--- Main games table --
+-- Main games table
 CREATE TABLE players.games(
     game_id BIGSERIAL PRIMARY KEY,
     a_id INT NOT NULL,
     b_id INT NOT NULL,
     c_id INT,  -- for doubles
     d_id INT,
+    points INT DEFAULT 21,  -- Can also be 11
     timestamp TIMESTAMP DEFAULT NOW(),
-    result INT NOT NULL DEFAULT 0,  -- 0 means a_id wins, 1 means b_id wins, 2 means draw
+    result INT DEFAULT 0,  -- 0 means a_id wins, 1 means b_id wins, 2 means draw
+    a_score INT DEFAULT 21,  -- Final score of winner
+    b_score INT,  -- Final score of loser
     notes VARCHAR(200),
-    tournament_game BOOLEAN DEFAULT FALSE,
+    -- tournament_id INT,
     FOREIGN KEY (a_id) REFERENCES players.users(user_id),
     FOREIGN KEY (b_id) REFERENCES players.users(user_id),
     FOREIGN KEY (c_id) REFERENCES players.users(user_id),
     FOREIGN KEY (d_id) REFERENCES players.users(user_id)
 );
 
--- History of ratings -
+-- History of ratings
 CREATE TABLE players.ratings(
     rating_id BIGSERIAL PRIMARY KEY,
     user_id INT NOT NULL,
+    game_id BIGINT NOT NULL,  -- Each rating is associated to a new game
     rating float NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES players.users(user_id)
+    FOREIGN KEY (user_id) REFERENCES players.users(user_id),
+    FOREIGN KEY (game_id) REFERENCES players.games(game_id)
 );
+
+-- TODO: Tournament table
+-- CREATE TABLE players.tournaments(
+--     tournament_id SERIAL PRIMARY KEY,
+
+-- );

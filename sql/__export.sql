0c54aa5a19684284013cb394061419c07f2ecec7
gamesguru <mathmuncher11@gmail.com>
2019-10-07 19:53:43 -0400

    update sql/__export.sql

--- sql/__export.sql
+++ sql/__export.sql
@@ -1,6 +1,10 @@
 
 \! echo '\nBEGIN: [__export.sql]';
 
+-- Players --
+\! echo '[Players]';
+\copy players.users TO 'players.csv' WITH (FORMAT csv, HEADER);
+
 -- Singles --
 \! echo '[Singles]';
 \copy players.singles_games TO 'scores_singles.csv' WITH (FORMAT csv, HEADER);

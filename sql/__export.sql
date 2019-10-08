dd04b5bf931a2170dc9a32ba1d243a0f0c265785
gamesguru <mathmuncher11@gmail.com>
2019-10-07 20:45:49 -0400

    update sql/__export.sql

--- sql/__export.sql
+++ sql/__export.sql
@@ -7,8 +7,8 @@
 
 -- Singles --
 \! echo '[Singles]';
-\copy players.singles_games TO 'scores_singles.csv' WITH (FORMAT csv, HEADER);
+\copy players.singles_games TO './data/scores_singles.csv' WITH (FORMAT csv, HEADER);
 
 -- Doubles --
 \! echo '[Doubles]';
-\copy players.singles_games TO 'scores_doubles.csv' WITH (FORMAT csv, HEADER);
+\copy players.singles_games TO './data/scores_doubles.csv' WITH (FORMAT csv, HEADER);

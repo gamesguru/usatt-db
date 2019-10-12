c78d41aec75067d41573103f04e141516126dc3d
gamesguru <mathmuncher11@gmail.com>
2019-10-12 13:36:36 -0400

    update user_ids

--- sql/onboard/functions.sql
+++ sql/onboard/functions.sql
@@ -57,14 +57,14 @@ LANGUAGE SQL;
 game_id,reporter_id,player1_id, player2_id,points,team1_score,timestamp,tournament_id
 
 SELECT
-  sg.game_id,u1.username AS player1,u2.username AS player2,sg.points,sg.team1_score,sg.timestamp,sg.tournament_id
+  sg.game_id,u1.username AS player1,u2.username AS player2,sg.points,sg.score1,sg.score2,sg.timestamp,sg.tournament_id
 FROM players.singles_games AS sg
 INNER JOIN players.users AS u1
   ON sg.player1_id=u1.user_id
 INNER JOIN players.users AS u2
   ON sg.player2_id=u2.user_id
 WHERE
-  sg.tournament_game='true';
+  sg.tournament_id=1;
 
 
 SELECT
@@ -75,7 +75,7 @@ INNER JOIN players.users AS u1
 INNER JOIN players.users AS u2
   ON sg.player2_id=u2.user_id
 WHERE
-  sg.tournament_game='true';
+  sg.tournament_id=1;
 
 
 

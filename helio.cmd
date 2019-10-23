517fdb3d69e190d98e7697b37c7e1788e69a55d7
gamesguru <mathmuncher11@gmail.com>
2019-10-22 22:36:04 -0400

    update helio.cmd

--- helio.cmd
+++ helio.cmd
@@ -1,13 +1,13 @@
 :;
 :;
 :; # A script to connect to remote HelioHost postgreSQL server
-:; # Set env vars:   PSQL_USATT_PASSWORD, PSQL_USATT_USER
+:; # Set env vars:   PSQL_USATT_PASSWORD, PSQL_USATT_USER, PSQL_USATT_DATABASE
 :;
 
 
 :; # -- BASH -- #
-:; psql postgresql://$PSQL_USATT_USER:$PSQL_USATT_PASSWORD@usatt.heliohost.org:5432/usatt_usatt
+:; psql postgresql://$PSQL_USATT_USER:$PSQL_USATT_PASSWORD@alephdog.heliohost.org:5432/$PSQL_USATT_DATABASE
 :; exit
 
 :; # - cmd.exe - #
-psql postgresql://%PSQL_USATT_USER%:%PSQL_USATT_PASSWORD%@usatt.heliohost.org:5432/usatt_usatt
+psql postgresql://%PSQL_USATT_USER%:%PSQL_USATT_PASSWORD%@alephdog.heliohost.org:5432/%PSQL_USATT_DATABASE%

c38f9866d5c6c90c7151e89b4c1c4f1cc7428c4c
gamesguru <mathmuncher11@gmail.com>
2019-10-23 22:03:04 -0400

    testing new season game generation

--- python/regular-season.py
+++ python/regular-season.py
@@ -17,7 +17,7 @@ smash_bros = [
     'adejans',
     'achintar',
     'gnurushe',
-    'vtalasan'
+    'vtalasan',
 ]
 back_comps = [
     'yhu60',
@@ -29,7 +29,10 @@ back_comps = [
     'aahir',
     'dvasamse',
     'sdeshmu2',
-    'kkesmia']
+    'kkesmia'
+]
+print(f'smash_bros: {len(smash_bros)}\tmembers')
+print(f'back_comps: {len(back_comps)}\tmembers\n')
 
 # Create games
 sb_games = []
@@ -42,12 +45,20 @@ for i in range(0, len(back_comps)):
     for j in range(i + 1, len(back_comps)):
         if i != j:
             bc_games.append(f'{back_comps[i].ljust(8)} vs {back_comps[j]}')
+mix_games = []
+for i in range(0, len(smash_bros)):
+    for j in range(0, len(back_comps)):
+        mix_games.append(f'{smash_bros[i].ljust(8)} vs {back_comps[j]}')
 
 # Print games
-print('==> Super Smash bros')
+print(f'==> Super Smash bros [3-GAME MATCHES] ({len(sb_games)})')
 for g in sb_games:
     print(g)
 
-print('\n==> Backhanded Compliments')
+print(f'\n==> Backhanded Compliments [3-GAME MATCHES] ({len(bc_games)})')
 for g in bc_games:
+    print(g)
+
+print(f'\n==> Mixed league [1 GAME DUELS] ({len(mix_games)})')
+for g in mix_games:
     print(g)

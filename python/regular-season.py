6c54ada3bdc1e49b38aa757892d0f55a30917d02
gamesguru <mathmuncher11@gmail.com>
2019-10-23 22:08:37 -0400

    update season games

--- python/regular-season.py
+++ python/regular-season.py
@@ -6,49 +6,55 @@ Created on Fri Oct 18 23:13:48 2019
 @author: shane
 """
 
-# Two conferences
-smash_bros = [
-    'pmathai',
-    'tmechine',
-    'rarora13',
-    'ychen310',
-    'sjaroch',
-    'gabhinav',
-    'adejans',
-    'achintar',
-    'gnurushe',
-    'vtalasan',
-]
-back_comps = [
-    'yhu60',
-    'rramac21',
-    'hmiriyam',
-    'choope20',
-    'eruff2',
-    'awhiten1',
-    'aahir',
-    'dvasamse',
-    'sdeshmu2',
-    'kkesmia'
-]
-print(f'smash_bros: {len(smash_bros)}\tmembers')
-print(f'back_comps: {len(back_comps)}\tmembers\n')
+league = {
+    # Two conferences
+    "smash_bros": [
+        'pmathai',
+        'tmechine',
+        'rarora13',
+        'ychen310',
+        'sjaroch',
+        'gabhinav',
+        'adejans',
+        'achintar',
+        'gnurushe',
+        'vtalasan',
+    ],
+    "back_comps": [
+        'yhu60',
+        'rramac21',
+        'hmiriyam',
+        'choope20',
+        'eruff2',
+        'awhiten1',
+        'aahir',
+        'dvasamse',
+        'sdeshmu2',
+        'kkesmia'
+    ]
+}
+
+print(f'smash_bros: {len(league["smash_bros"])}\tmembers')
+print(f'back_comps: {len(league["back_comps"])}\tmembers\n')
 
 # Create games
 sb_games = []
-for i in range(0, len(smash_bros)):
-    for j in range(i + 1, len(smash_bros)):
+for i in range(0, len(league["smash_bros"])):
+    for j in range(i + 1, len(league["smash_bros"])):
         if i != j:
-            sb_games.append(f'{smash_bros[i].ljust(8)} vs {smash_bros[j]}')
+            sb_games.append(
+                f'{league["smash_bros"][i].ljust(8)} vs {league["smash_bros"][j]}')
 bc_games = []
-for i in range(0, len(back_comps)):
-    for j in range(i + 1, len(back_comps)):
+for i in range(0, len(league["back_comps"])):
+    for j in range(i + 1, len(league["back_comps"])):
         if i != j:
-            bc_games.append(f'{back_comps[i].ljust(8)} vs {back_comps[j]}')
+            bc_games.append(
+                f'{league["back_comps"][i].ljust(8)} vs {league["back_comps"][j]}')
 mix_games = []
-for i in range(0, len(smash_bros)):
-    for j in range(0, len(back_comps)):
-        mix_games.append(f'{smash_bros[i].ljust(8)} vs {back_comps[j]}')
+for i in range(0, len(league["smash_bros"])):
+    for j in range(0, len(league["back_comps"])):
+        mix_games.append(
+            f'{league["smash_bros"][i].ljust(8)} vs {league["back_comps"][j]}')
 
 # Print games
 print(f'==> Super Smash bros [3-GAME MATCHES] ({len(sb_games)})')

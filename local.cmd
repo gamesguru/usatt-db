f8a23ad0ea25bb8f834cbfff3055006c4f01a320
gamesguru <mathmuncher11@gmail.com>
2019-11-02 06:14:48 -0700

    fix local.cmd on Windows

--- local.cmd
+++ local.cmd
@@ -12,7 +12,10 @@
 :; psql postgresql://$LOGNAME@localhost:5432/usatt
 :; exit
 
+
+:; # Note: Must create db usatt from /postgres
+:; # Note: On Windows, must create superuser "Shane" or %USERNAME% from php my pg admin
 :; # - cmd.exe - #
 pg_ctl initdb -D %PSQL_USATT_LOCAL_DB_DIR% -l %PSQL_USATT_LOCAL_DB_DIR%/postgreslogfile
 pg_ctl -D %PSQL_USATT_LOCAL_DB_DIR% -l %PSQL_USATT_LOCAL_DB_DIR%/postgreslogfile start
-psql postgresql://%USERNAME%@localhost:5432/usatt
+psql postgresql://%USERNAME%:password@localhost:5432/usatt
